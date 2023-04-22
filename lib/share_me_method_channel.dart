d09abd7725dc325204115418ab453964411e0034
TheMonstersApp Dev <developer@themonstersapp.com>
2023-04-22 17:12:27 -0400

    Fix some issue with License

--- lib/share_me_method_channel.dart
+++ lib/share_me_method_channel.dart
@@ -11,7 +11,8 @@ class MethodChannelShareMe extends ShareMePlatform {
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
 }
