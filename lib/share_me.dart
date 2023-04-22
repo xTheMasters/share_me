f8f3e51a753f5a7b84633c7546af1fc6adf46d28
TheMonstersApp Dev <developer@themonstersapp.com>
2023-04-22 17:13:11 -0400

    Cleanning some code

--- lib/share_me.dart
+++ lib/share_me.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/services.dart';
 
 class ShareMe {
@@ -36,30 +35,5 @@ class ShareMe {
     };
 
     await _channel.invokeMethod('share_me_file', args);
-  }
-
-  static Future<void> files({
-    required String name,
-    required String mimeType,
-    required List<Uint8List> imageDataList,
-  }) async {
-    assert(name.isNotEmpty, "El nombre del archivo no puede ser nulo o vacío.");
-    assert(mimeType.isNotEmpty, "El tipo MIME no puede ser nulo o vacío.");
-
-    List<Uint8List> imageDataListCopy = List.from(imageDataList);
-    List<String> imageDataListBase64 = [];
-
-    for (Uint8List imageData in imageDataListCopy) {
-      String imageDataBase64 = base64Encode(imageData);
-      imageDataListBase64.add(imageDataBase64);
-    }
-
-    Map<String, dynamic> args = {
-      'name': name,
-      'mimeType': mimeType,
-      'imageData': imageDataListBase64,
-    };
-
-    await _channel.invokeMethod('share_me_files', args);
   }
 }
